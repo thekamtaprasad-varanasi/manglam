056171e06cc1202ea221691227340a2d6ab18cb5
kp1153 <kamta_tiwari@yahoo.com>
2026-06-01 15:06:20 +0530

    force deploy

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,7 +4,7 @@ export default {
   schema: "./lib/schema.js",
   out: "./drizzle",
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN,
+    url: "libsql://manglam-manglam.aws-ap-south-1.turso.io",
+    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODAzMDM2MjIsImlkIjoiMDE5ZTgyNTctMmIwMS03OTIyLWI5MGYtM2FkNzUwZDFmM2NhIiwicmlkIjoiMmUwY2QyYmItNTE2Yi00ZGY0LWI1YWUtMDU5YzY2MjU0YWYzIn0.GuT8B60I9rFJpnb3OcJcoZVoUj7rD6n3M3l759hSujLmCyDbCeUgmvdjV_mwqJzjBpm9JDl6vdlVaDfH_pFtDQ",
   },
 };
